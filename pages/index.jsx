4f376b29a7fb9253ae729bc7a42fc582696a34c8
SandeepPabraGit <110338326+SandeepPabraGit@users.noreply.github.com>
2022-09-25 10:24:36 +0530

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,28 @@
+
+
+import React, { Fragment } from 'react'
+import About from './components/UI/About';
+import Contact from './components/UI/Contact';
+import Hero from './components/UI/Hero';
+import Portfolio from './components/UI/Portfolio';
+import Service from './components/UI/Service';
+import Testimonial from './components/UI/Testimonial';
+
+
+
+function index() {
+  return <Fragment>
+  <Hero/>
+  <Service/>
+
+<About/>
+
+<Portfolio />
+<Testimonial />
+
+<Contact/>
+
+  </Fragment>
+}
+
+export default index;
